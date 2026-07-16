844258dedb57f02eb850cd9c9d397c2b8ad68d73
Maryam Siddique <siddiquemaryam32@gmail.com>
2026-07-16 16:42:32 +0500

    Fix Netlify build

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,5 +1,15 @@
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:`pwd`/flutter/bin"
-flutter doctor
+#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+
+export PATH="$(pwd)/flutter/bin:$PATH"
+
+flutter --version
+dart --version
+
+flutter config --enable-web
+
 flutter pub get
-flutter build web
+
+flutter build web --release
